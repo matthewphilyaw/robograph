0ac0164f19d607dba628269fe4c25f5f17ef7d95
matthew philyaw <matthew.philyaw@gmail.com>
2013-07-21 10:08:44 -0400

    fixed a few issues, and fixed formatting.

--- classes/graph.py
+++ classes/graph.py
@@ -1,19 +1,19 @@
 class Graph:
-	def __init__(self):
-		self.nodes = {}
+    def __init__(self):
+        self.nodes = {}
 
-	def find_node(self, heading):
-		if heading in self.nodes:
-			return self.nodes[heading]
+    def find_node(self, heading):
+        if heading in self.nodes:
+            return self.nodes[heading]
 
-		return None
+        return None
 
-	def add_node(self, node):
-		if node.data.heading in self.nodes:
-			raise Exception("node already exists")
+    def add_node(self, node):
+        if node.data.heading in self.nodes:
+            raise Exception("node already exists")
 
-		self.nodes[node.data.heading] = node
+        self.nodes[node.data.heading] = node
 
-	def remove_node(self, node):
-		node.remove_all_edges()	
-		del self.nodes[node]
+    def remove_node(self, node):
+        node.remove_all_edges()
+        del self.nodes[node]
--- classes/node.py
+++ classes/node.py
@@ -1,58 +1,58 @@
 class Node:
-	def __init__(self, data):
-		self.incoming_edges = {}
-		self.outgoing_edges = {} 
-		self.data = data
+    def __init__(self, data):
+        self.incoming_edges = {}
+        self.outgoing_edges = {}
+        self.data = data
 
-	def add_outgoing_edge(self, node, meta):
-		"""
-			Inserts outgoing edge if doesn't exist with meta data
-			else does nothing
-		"""
-		_update_dict(self.outgoing_edges, node, meta)
+    def add_outgoing_edge(self, node, meta):
+        """
+            Inserts outgoing edge if doesn't exist with meta data
+            else does nothing
+        """
+        self._update_dict(self.outgoing_edges, node, meta)
 
-	def add_incoming_edge(self, node, meta):
-		"""
-			Inserts outgoing edge if doesn't exist with meta data
-			else does nothing
-		"""
-		_update_dict(self.incoming_edges, node, meta)
+    def add_incoming_edge(self, node, meta):
+        """
+            Inserts outgoing edge if doesn't exist with meta data
+            else does nothing
+        """
+        self._update_dict(self.incoming_edges, node, meta)
 
-	def _update_dict(self, dict, node, meta):
-		if node not in dict:
-			dict[node] = meta
+    def _update_dict(self, dict, node, meta):
+        if node not in dict:
+            dict[node] = meta
 
-	def remove_edge(self, node):
-		if node == None:
-			return
-			
-		if node != self:
-			node.remove_edge(self)
+    def remove_edge(self, node):
+        if node == None:
+            return
 
-		if node in self.incoming_edges:
-			del self.incoming_edges[node]
+        if node != self:
+            node.remove_edge(self)
 
-		if node in self.outgoing_edges:
-			del self.outgoing_edges[node]
+        if node in self.incoming_edges:
+            del self.incoming_edges[node]
 
-	def remove_all_edges(self):
-		for k,v in self.outgoing_edges.items():
-			k.remove_edge(self)
-			del self.outgoing_edges[k]
-		for k,v in self.incoming_edges.items():
-			k.remove_edge(self)
-			del self.outgoing_edges[k]
+        if node in self.outgoing_edges:
+            del self.outgoing_edges[node]
 
-	def get_incoming_edge_data(self, node):
-		if node in self.incoming_edges:
-			return self.incoming_edges[node]
+    def remove_all_edges(self):
+        for k,v in self.outgoing_edges.items():
+            k.remove_edge(self)
+            del self.outgoing_edges[k]
+        for k,v in self.incoming_edges.items():
+            k.remove_edge(self)
+            del self.outgoing_edges[k]
 
-	def get_incoming_edges(self):
-		return self.incoming_edges.items()
+    def get_incoming_edge_data(self, node):
+        if node in self.incoming_edges:
+            return self.incoming_edges[node]
 
-	def get_outgoing_edge_data(self, node):
-		if node in self.outgoing_edges:
-			return self.outgoing_edges[node]
+    def get_incoming_edges(self):
+        return self.incoming_edges.items()
 
-	def get_outgoing_edges(Self):
-		return self.outgoing_edges.items()
+    def get_outgoing_edge_data(self, node):
+        if node in self.outgoing_edges:
+            return self.outgoing_edges[node]
+
+    def get_outgoing_edges(self):
+        return self.outgoing_edges.items()
